84681e785810be2ec7bbe945f60c81ae8e369436
Chandrakanta Behera <chandan16460@gmail.com>
2024-03-23 11:46:54 +0530

    Initial Commit

---
+++ play/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  play
+//
+//  Created by Mac on 23/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ play/playApp.swift
@@ -0,0 +1,17 @@
+//
+//  playApp.swift
+//  play
+//
+//  Created by Mac on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

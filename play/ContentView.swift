cb3fd6a276bc613f9cbdcb70d8956e6b20705287
Chandrakanta Behera <chandan16460@gmail.com>
2024-03-23 14:17:46 +0530

    base layout of home page added

--- play/ContentView.swift
+++ play/ContentView.swift
@@ -1,24 +1,57 @@
-//
-//  ContentView.swift
-//  play
-//
-//  Created by Mac on 23/03/24.
-//
-
 import SwiftUI
 
+struct Movie: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
+struct SectionItem: Identifiable {
+    var id = UUID()
+    var title: String
+    var movies: [Movie]
+}
+
 struct ContentView: View {
+    let sectionData = [
+        SectionItem(title: "Comedy", movies: [Movie(title: "Movie 1"), Movie(title: "Movie 2"), Movie(title: "Movie 3")]),
+        SectionItem(title: "Action", movies: [Movie(title: "Movie 4"), Movie(title: "Movie 5"), Movie(title: "Movie 6")]),
+        SectionItem(title: "Drama", movies: [Movie(title: "Movie 7"), Movie(title: "Movie 8"), Movie(title: "Movie 9")])
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(sectionData) { section in
+            Section(header: Text(section.title)) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(section.movies) { movie in
+                            MovieItem(movie: movie)
+                        }
+                    }
+                    .padding()
+                }
+            }
         }
-        .padding()
+        .listStyle(InsetGroupedListStyle())
     }
 }
 
-#Preview {
-    ContentView()
+struct MovieItem: View {
+    let movie: Movie
+    
+    var body: some View {
+        VStack {
+            Text(movie.title)
+                .padding()
+                .frame(width: 150, height: 200)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
